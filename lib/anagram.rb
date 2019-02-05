8aba1e27e6ad04cc8ee50c00d35c6db70a692628
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-05 20:09:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,15 @@
 class Anagram
   
-  attr_accessor :match, :word
+  attr_accessor :word
   
   def initialize(word)
   @word = word
+  end
+  
+  def @match(list)
+    list.split(" ").each do |w| if w.sort == word.sort return w else []
+  end 
+end
   end
   
   
